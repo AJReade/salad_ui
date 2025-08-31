b95471d13f799c1136555a0b657f35c4911db503
AJReade <74275743+AJReade@users.noreply.github.com>
2025-08-31 15:21:06 +0100

    added saladui local dep to testing example

--- example/mix.exs
+++ example/mix.exs
@@ -65,7 +65,8 @@ defmodule Example.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:salad_ui, path: ".."}
     ]
   end
 
